2662575cf45d6a107ee16f3d16a932448933d7b6
Les Kleuver <les.kleuver@gmail.com>
2010-11-19 23:26:54 +0100

    git add -A fixed it

--- index.php
+++ index.php
@@ -1,15 +1,7 @@
 <?php
+
+include 'config.local.php';
 include "app/Stallcount9.php";
 
-$_s = array();
-$_s["database"] = array();
-$_s["database"]["type"]		= Stallcount9::DB_MYSQL;
-$_s["database"]["user"] 	= "les";
-$_s["database"]["password"]	= "bla";
-$_s["database"]["host"]		= "127.0.0.1";
-$_s["database"]["name"]		= "stallcount9";
-
-$_s["skin"] = "default";
-
-$sc9 = new Stallcount9($_s);
+$sc9 = new Stallcount9($config);
 $sc9->handleRequests($_REQUEST);
