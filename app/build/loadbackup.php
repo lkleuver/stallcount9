29435d31930c72235141d5faadd571c94a354878
Les Kleuver <les.kleuver@gmail.com>
2011-04-25 16:05:38 +0200

    new?

--- app/build/loadbackup.php
+++ app/build/loadbackup.php
@@ -4,7 +4,8 @@ require_once('bootstrap.php');
 
 echo "Loading data...\n";
 
+$file = count($argv) > 0 ? $argv[0] : "fixtures/backup.yml";
 
 Doctrine_Core::debug(true);
 Doctrine_Core::loadModels($modelsPath);
-Doctrine_Core::loadData('fixtures/backup.yml');
+Doctrine_Core::loadData('fixtures/'.$file);
